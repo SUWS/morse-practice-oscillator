518b0e7e0f2068054f87d8acbaf63a2550fb8577
Tyler Ward <tyler@scorpia.co.uk>
2018-05-01 22:16:36 +0100

    Added more test code to test the ADC and digital inputs.
    Also added basic uart printing for debug output.

--- firmware/morse_code_avr.c
+++ firmware/morse_code_avr.c
@@ -47,9 +47,57 @@ uint16_t adc_read(uint8_t ch)// [2]
 	return ADC;
 }
 
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+
+uint16_t uart_out_string(char* str)
+{
+	uint16_t length = strlen(str);
+	
+	int i=0;
+	for(i=0;i<length;i++)
+	{
+	  // Wait until last byte has been transmitted
+	  while((UCSR0A &(1<<UDRE0)) == 0);
+
+	  // Transmit data
+	  UDR0 = str[i];
+	}
+}
+
+/**************************************/
+/*! Converts and ADC reading into a dial position
+ *
+ * The dials are non linear so the values have been calculated based on observed readings.
+ * \param val ADC value from the dial
+ * \retval dial position from 0 to 10
+ */
+int adc_pos(int val)
+{
+	if(val<15) return 0;
+	if(val<88) return 1;
+	if(val<198)	return 2;
+	if(val<314)	return 3;
+	if(val<438) return 4;
+	if(val<577) return 5;
+	if(val<702) return 6;
+	if(val<817) return 7;
+	if(val<929) return 8;
+	if(val<1010) return 9;
+	return 10;	
+}
+
+
 
 int main(int argc, char const *argv[])
 {
+	adc_init();
+	// Set baud rate
+	UBRR0L = BAUD_PRESCALE;// Load lower 8-bits into the low byte of the UBRR register
+	UBRR0H = (BAUD_PRESCALE >> 8); 
+	// Enable receiver and transmitter and receive complete interrupt 
+	UCSR0B = ((1<<TXEN0)|(1<<RXEN0));
+	
 	// Setting PC2(dot) & PC3(dash) as digital inputs
 	KEY_IN_PORT_DDR &= ~_BV(LED_DOT);
 	KEY_IN_PORT_DDR &= ~_BV(LED_DASH);
@@ -92,6 +140,54 @@ int main(int argc, char const *argv[])
 		INDICATOR_LED_PORT |= _BV(INDICATOR_LED_GREEN);
 		_delay_ms(1000);
 		INDICATOR_LED_PORT &= ~_BV(INDICATOR_LED_GREEN);
+		
+		char tmp[24];
+		int adcval = adc_read(ADC_WEIGHT);
+		sprintf(tmp,"Weight :%d (%d)\r\n",adc_pos(adcval),adcval);
+		//sprintf(tmp,"Weight :%d\r\n",adc_read(ADC_WEIGHT));
+		uart_out_string(tmp);
+		
+		adcval = adc_read(ADC_RATE);
+		sprintf(tmp,"Rate :%d (%d)\r\n",adc_pos(adcval),adcval);
+		//sprintf(tmp,"Rate :%d\r\n",adc_read(ADC_RATE));
+		uart_out_string(tmp);
+		
+		adcval = adc_read(ADC_VOLUME);
+		sprintf(tmp,"Volume :%d (%d)\r\n",adc_pos(adcval),adcval);
+		//sprintf(tmp,"Rate :%d\r\n",adc_read(ADC_RATE));
+		uart_out_string(tmp);
+		
+		adcval = adc_read(ADC_TONE);
+		sprintf(tmp,"Tone :%d (%d)\r\n",adc_pos(adcval),adcval);
+		//sprintf(tmp,"Rate :%d\r\n",adc_read(ADC_RATE));
+		uart_out_string(tmp);
+		
+		adcval = adc_read(AMP_DEBUG);
+		sprintf(tmp,"Amp Debug :%d (",adcval);
+		//sprintf(tmp,"Rate :%d\r\n",adc_read(ADC_RATE));
+		uart_out_string(tmp);
+		if(adcval<128) uart_out_string("OUTL to VBAT)\r\n");
+		else if(adcval<384) uart_out_string("OUTR to VBAT)\r\n");
+		else if(adcval<640) uart_out_string("OUTL to GND)\r\n");
+		else if(adcval<896) uart_out_string("OUTR to GND)\r\n");
+		else uart_out_string("NO Fault)");
+		
+		int switchPos = (KEY_MODE_PORT_PIN >> KEY_MODE)&0x01;
+		sprintf(tmp,"Switch :%d \r\n",switchPos);
+		uart_out_string(tmp);
+		
+		int keyPos = (KEY_IN_PORT_PIN >> KEY_DOT)&0x01;
+		sprintf(tmp,"Dot :%d \r\n",keyPos);
+		uart_out_string(tmp);
+		
+		keyPos = (KEY_IN_PORT_PIN >> KEY_DASH)&0x01;
+		sprintf(tmp,"Dash :%d \r\n",keyPos);
+		uart_out_string(tmp);
+		
+		uart_out_string("#####\r\n");
+		_delay_ms(1000);
+		
+		
 	}
 
 	return 0;
